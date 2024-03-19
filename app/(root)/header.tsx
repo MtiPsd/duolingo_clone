c2938c525bddb931647ba1d718cbaabab47b8268
Mahdi Pasandideh <mtipsd.git@gmail.com>
2024-03-19 23:35:21 +0330

    Added :: necessary assets

--- app/(root)/header.tsx
+++ app/(root)/header.tsx
@@ -1,7 +1,21 @@
+import Image from "next/image";
+
 export function Header() {
   return (
     <header className="w-full h-20 px-4 border-b-2 border-slate-200">
-      Header
+      <div className="flex items-center justify-between h-full mx-auto lg:max-w-screen-lg">
+        <div className="flex items-center pt-8 pl-7 pb-7 gap-x-3">
+          <Image
+            src="/mascot.svg"
+            height={40}
+            width={40}
+            alt="Mascot"
+          />
+          <h1 className="text-2xl font-extrabold tracking-wide text-green-600">
+            Lingo
+          </h1>
+        </div>
+      </div>
     </header>
   );
 }
