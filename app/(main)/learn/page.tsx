b4f60ad13f134ca535996cfeb6125d9a290fc3b9
Mahdi Pasandideh <mtipsd.git@gmail.com>
2024-03-31 21:35:49 +0330

    Created :: header layout for learn page

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,11 +1,14 @@
 import { FeedWrapper } from "@/components/feed_wrapper";
 import { StickyWrapper } from "@/components/sticky_wrapper";
+import { Header } from "./header";
 
 function LearnPage() {
   return (
     <div className="flex flex-row-reverse gap-[48px] px-6">
       <StickyWrapper>My Sticky Sidebar</StickyWrapper>
-      <FeedWrapper>My Wrapper</FeedWrapper>
+      <FeedWrapper>
+        <Header title="Spanish" />
+      </FeedWrapper>
     </div>
   );
 }
