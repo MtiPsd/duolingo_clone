da1f78a66b56c8ec0129c693ec84ce21277c8c31
Mahdi Pasandideh <mtipsd.git@gmail.com>
2024-03-31 21:08:55 +0330

    Created :: Feed and Sticky sidebar layout

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,5 +1,13 @@
+import { FeedWrapper } from "@/components/feed_wrapper";
+import { StickyWrapper } from "@/components/sticky_wrapper";
+
 function LearnPage() {
-  return <div>Learn</div>;
+  return (
+    <div className="flex flex-row-reverse gap-[48px] px-6">
+      <StickyWrapper>My Sticky Sidebar</StickyWrapper>
+      <FeedWrapper>My Wrapper</FeedWrapper>
+    </div>
+  );
 }
 
 export default LearnPage;
