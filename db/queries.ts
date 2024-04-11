ed4ca2e4c19901fd008bd07e3c8e6649764547ec
Mahdi Pasandideh <mtipsd.git@gmail.com>
2024-04-11 21:18:37 +0330

    Added :: getUserProgress query

--- db/queries.ts
+++ db/queries.ts
@@ -1,7 +1,25 @@
 import { cache } from "react";
 import db from "@/db/drizzle";
+import { auth } from "@clerk/nextjs";
+import { eq } from "drizzle-orm";
+import { userProgress } from "@/db/schema";
 
 export const getCourses = cache(async () => {
   const data = await db.query.courses.findMany();
+  return data;
+});
+
+export const getUserProgress = cache(async () => {
+  const { userId } = auth();
+
+  if (!userId) return null;
+
+  const data = db.query.userProgress.findFirst({
+    where: eq(userProgress.userId, userId),
+    // populate active course relation
+    with: {
+      activeCourse: true,
+    },
+  });
   return data;
 });
