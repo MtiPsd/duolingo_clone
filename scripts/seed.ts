05fd4c97e0708b88631eabc406eeb45ef887b22c
Mahdi Pasandideh <mtipsd.git@gmail.com>
2024-04-17 16:42:32 +0330

    Updated :: courses hearts, points and title with flag to be dynamic

--- scripts/seed.ts
+++ scripts/seed.ts
@@ -13,6 +13,29 @@ async function main() {
     await db.delete(schema.courses);
     await db.delete(schema.userProgress);
 
+    await db.insert(schema.courses).values([
+      {
+        id: 1,
+        title: "Spanish",
+        imageSrc: "/es.svg",
+      },
+      {
+        id: 2,
+        title: "Italian",
+        imageSrc: "/it.svg",
+      },
+      {
+        id: 3,
+        title: "French",
+        imageSrc: "/fr.svg",
+      },
+      {
+        id: 4,
+        title: "Croatian",
+        imageSrc: "/hr.svg",
+      },
+    ]);
+
     console.log("Seeding Finished");
   } catch (error) {
     console.error(error);
