309eb9ff92b499e8b89d27babd10b1ec045e4060
Mahdi Pasandideh <mtipsd.git@gmail.com>
2024-04-20 19:23:22 +0330

    Finished :: UnitBanner component

--- app/(main)/learn/unit_banner.tsx
+++ app/(main)/learn/unit_banner.tsx
@@ -1,3 +1,7 @@
+import { Button } from "@/components/ui/button";
+import { NotebookText } from "lucide-react";
+import Link from "next/link";
+
 type Props = {
   title: string;
   description: string;
@@ -6,10 +10,21 @@ type Props = {
 function UnitBanner({ description, title }: Props) {
   return (
     <div className="w-full rounded-xl bg-green-500 p-5 text-white flex items-center justify-between">
-      <div className="space-y-2.5">
-        <h1>{title}</h1>
-        <p>{description}</p>
+      <div className="">
+        <h3 className="text-2xl font-bold">{title}</h3>
+        <p className="text-lg">{description}</p>
       </div>
+
+      <Link href="/lesson">
+        <Button
+          size="lg"
+          variant="secondary"
+          className="hidden xl:flex border-2 border-b-4 active:border-b-2"
+        >
+          <NotebookText className="mr-2 " />
+          continue
+        </Button>
+      </Link>
     </div>
   );
 }
