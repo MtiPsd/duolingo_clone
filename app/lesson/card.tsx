711ac0ccd9fc9b4cd01d3c1e9b4d6f8fd8603650
Mahdi Pasandideh <mtipsd.git@gmail.com>
2024-04-21 22:01:20 +0330

    Finished :: adding voice feature

--- app/lesson/card.tsx
+++ app/lesson/card.tsx
@@ -1,6 +1,8 @@
 import { challenges } from "@/db/schema";
 import { cn } from "@/lib/utils";
 import Image from "next/image";
+import { useCallback } from "react";
+import { useAudio, useKey } from "react-use";
 
 type Props = {
   id: number;
@@ -8,15 +10,15 @@ type Props = {
   imageSrc: string | null;
   shortcut: string;
   selected?: boolean;
-  onClick: (id: number) => void;
+  onClick: () => void;
   status?: "correct" | "wrong" | "none";
-  audioStc: string | null;
+  audioSrc: string | null;
   disabled?: boolean;
   type: typeof challenges.$inferSelect.type;
 };
 
 export function Card({
-  audioStc,
+  audioSrc,
   disabled,
   id,
   imageSrc,
@@ -27,9 +29,20 @@ export function Card({
   text,
   type,
 }: Props) {
+  const [audio, _, controls] = useAudio({ src: audioSrc || "" });
+
+  const handleClick = useCallback(() => {
+    if (disabled) return;
+
+    controls.play();
+    onClick();
+  }, [controls, disabled, onClick]);
+
+  useKey(shortcut, handleClick, {}, [handleClick]);
+
   return (
     <div
-      onClick={() => {}}
+      onClick={handleClick}
       className={cn(
         "h-full border-2 rounded-xl border-b-4 hover:bg-black/5 p-4 lg:p-6 cursor-pointer active:border-b-2",
         selected && "border-sky-300 bg-sky-100 hover:bg-sky-100",
@@ -43,6 +56,7 @@ export function Card({
         type == "ASSIST" && "lg:p-3 w-full",
       )}
     >
+      {audio}
       {imageSrc && (
         <div className="relative aspect-square mb-4 max-h-[80px] lg:max-h-[150px] w-full">
           <Image src={imageSrc} alt={text} fill />
