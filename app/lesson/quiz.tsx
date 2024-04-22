546366946482a55ac6c4144e97d3898cb50f1907
Mahdi Pasandideh <mtipsd.git@gmail.com>
2024-04-22 20:34:37 +0330

    Added :: sounds

--- app/lesson/quiz.tsx
+++ app/lesson/quiz.tsx
@@ -9,6 +9,7 @@ import { Footer } from "./footer";
 import { upsertChallengeProgress } from "@/actions/challenge_progress";
 import { toast } from "sonner";
 import { reduceHearts } from "@/actions/user_progress";
+import { useAudio } from "react-use";
 
 type Props = {
   initialLessonId: number;
@@ -28,6 +29,12 @@ export function Quiz({
   initialPercentage,
   userSubscription,
 }: Props) {
+  const [correctAudio, _c, correctControls] = useAudio({
+    src: "/correct.wav",
+  });
+  const [incorrectAudio, _i, incorrectControls] = useAudio({
+    src: "/incorrect.wav",
+  });
   const [isPending, startTransition] = useTransition();
 
   const [hearts, setHearts] = useState(initialHearts);
@@ -91,6 +98,7 @@ export function Quiz({
               return;
             }
 
+            correctControls.play();
             setStatus("correct");
             setPercentage(prev => prev + 100 / challenges.length);
 
@@ -112,6 +120,7 @@ export function Quiz({
               return;
             }
 
+            incorrectControls.play();
             setStatus("wrong");
 
             if (!res?.error) {
@@ -127,6 +136,9 @@ export function Quiz({
 
   return (
     <>
+      {correctAudio}
+      {incorrectAudio}
+
       <Header
         hearts={hearts}
         percentage={percentage}
