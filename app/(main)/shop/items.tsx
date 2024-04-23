c9ae4aaa380e560b323795f1b68e2e473760b46a
Mahdi Pasandideh <mtipsd.git@gmail.com>
2024-04-23 21:22:07 +0330

    Start :: creating refill hearts action

---
+++ app/(main)/shop/items.tsx
@@ -0,0 +1,63 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import { useTransition } from "react";
+
+const POINTS_TO_REFILL = 10;
+
+type Props = {
+  hearts: number;
+  points: number;
+  hasActiveSubscription: boolean;
+};
+
+export function Items({
+  hasActiveSubscription,
+  hearts,
+  points,
+}: Props) {
+  const [isPending, startTransition] = useTransition();
+
+  function onRefillHearts() {
+    if (isPending || hearts === 5 || points < POINTS_TO_REFILL) {
+      return;
+    }
+
+    startTransition(() => {});
+  }
+
+  return (
+    <ul className="w-full">
+      <div className="flex items-center w-full p-4 gap-x-4 border-t-2">
+        <Image src="/heart.svg" alt="Heart" height={60} width={60} />
+        <div className="flex-1">
+          <p className="text-neutral-700 text-base lg:text-xl font-bold">
+            Refill hearts
+          </p>
+        </div>
+
+        <Button
+          onClick={onRefillHearts}
+          disabled={
+            isPending || hearts === 5 || points < POINTS_TO_REFILL
+          }
+        >
+          {hearts === 5 ? (
+            "full"
+          ) : (
+            <div className="flex items-center">
+              <Image
+                src="/points.svg"
+                alt="Points"
+                height={20}
+                width={20}
+              />
+              <p>{POINTS_TO_REFILL}</p>
+            </div>
+          )}
+        </Button>
+      </div>
+    </ul>
+  );
+}
